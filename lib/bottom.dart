fb177ff2bac906338a984e62f9d932addc00f882
rwema3 <rwemadominik3@gmail.com>
2022-02-20 20:53:10 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -1,12 +1,5 @@
 library bottom_navy_bar;
-e selected item.
-/// [selectedIndex] is rredmust t be null.
-class BottomvyBar onst _ItemWidget({
-
-  @override
-  Widget 
-        height
-              isSelected ? item.activeColor.withOpacity(0.2) : backgroundColor,
+ted ? item.activeColor.withOpacity(0.2) : backgroundColor,
           borderRadius: BorderRadius.circular(itemCornerRadius),
         ),
         child: SingleChildScrollView(
