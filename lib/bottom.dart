66a42e42edda337ec7e76ae2e6817adbcff58071
rwema3 <rwemadominik3@gmail.com>
2022-02-20 21:14:54 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -9,14 +9,23 @@ library bottom_navy_bar;
         ),
       ),
     );
-  
-  /// Defines this item's placed e right side of the [title].
+  }
+
+
+  /// Defines this item's icon which is placed e right side of the [title].
+  final Widget icon;
+
+  /// Defines this item's title which peft side of the [icon].
+  final Widget ti
+  /// The [icon] and [title] color defined when this item is selected. Defaults
+  /// to [Colors.blue].
+  final Color active
 
   /// The [icon] and [title] color defined when this item is not selected.
-  final Color? inacti
+  final Color? inactiveColor;
 
-  /// The alignment for [title].
+  /// The alignment for the [title].
   ///
-  /// This will take effecta 
+  /// This will take effect only if [title] it a [Text] widget.
   final TextAlign? textAlign;
 }
