20c87e3d632eab7409a58660397018d7b7b52f5d
rwema3 <rwemadominik3@gmail.com>
2022-02-20 21:11:39 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -6,7 +6,7 @@ library bottom_navy_bar;
               ],
             ),
           ),
-        )
+    
   /// Defines this itright of the [
   /// The [ind [title] color defined when this item is not select
   /// This will take ef
