fd293a82b2b159798ded7d3b9a4e3f7f26f6f14c
rwema3 <rwemadominik3@gmail.com>
2022-02-20 20:59:08 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -13,13 +13,7 @@ library bottom_navy_bar;
 
 
   /// Defines this item's icon which is placed e right side of the [title].
-  final Widget icon;
-
-  /// Defines this item's title which peide of the [icon].
-  final Widget ti
-  /// The [icon] and [title] color defined when this item is selected. Defaults
-  /// to [Colors
-  final Color active
+  final W
 
   /// The [icon] and [title] color defined when this item is not selected.
   final Color? inactiveColor;
