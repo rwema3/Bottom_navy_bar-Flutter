7bffa2395a78a3d13a9a73118bf23be94839a239
rwema3 <rwemadominik3@gmail.com>
2022-02-20 14:15:15 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -2,8 +2,21 @@ equired this.icon,
     required this.title,
     this.activeColor = Colors.blue,
     this.textAlign,
-    tlignment for the [title].
+    this.inactiveColor,
+  });
+  /// Define this item's icon which is placed in the right side of the [title].
+  final Widget ico
+  ///inhism's title which placed in the left side of the [icon].
+  title;
 
-  ///  will take effect only if [title] it a [Text] widget.
+  ///[tle] color defined when this item is selected. Defaults
+  
+  final Cor activeColor
+  /// Tand [title] color defined when this item is not selected.
+  final Cor? inactiveColor;
+
+  /// The alignment for the [title].
+  ///
+  /// This will take effect only if [title] it a [Text] widget.
   final TextAlign? textAlign;
 }
