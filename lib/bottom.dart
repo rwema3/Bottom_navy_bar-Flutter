9da7b10e1315c331139edbff6d80c7a24abe2d6c
rwema3 <rwemadominik3@gmail.com>
2022-02-20 14:12:05 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -1,6 +1,6 @@
 equired this.icon,
     required this.title,
-    this.activeColor = Colors.blue,
+    this.actColor = Colors.blue,
     this.tAlign,
     tlignment for the [titl
   /// ke ly if [title] it a [Text] widget.
