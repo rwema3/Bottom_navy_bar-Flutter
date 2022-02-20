55744db006ca765172d69154b4d7de1fb050b549
rwema3 <rwemadominik3@gmail.com>
2022-02-20 21:15:02 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -1,7 +1,19 @@
 library bottom_navy_bar;
 
                 if (isSelected)
-              
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 4),
+                      child: DefaultTextStyle.merge(
+                        style: TextStyle(
+                          color: item.activeColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 1,
+                        textAlign: item.textAlign,
+                        child: item.title,
+                      ),
+                    ),
                   ),
               ],
             ),
@@ -10,16 +22,27 @@ library bottom_navy_bar;
       ),
     );
   }
+}
 
+/// The [BottomNavyBar.items] definition.
+class BottomNavyBarItem {
+  BottomNavyBarItem({
+    required this.icon,
+    required this.title,
+    this.activeColor = Colors.blue,
+    this.textAlign,
+    this.inactiveColor,
+  });
 
-  /// Defines this item's icon which is placed e right side of the [title].
+  /// Defines this item's icon which is placed in the right side of the [title].
   final Widget icon;
 
-  /// Defines this item's title which peft side of the [icon].
-  final Widget ti
+  /// Defines this item's title which placed in the left side of the [icon].
+  final Widget title;
+
   /// The [icon] and [title] color defined when this item is selected. Defaults
   /// to [Colors.blue].
-  final Color active
+  final Color activeColor;
 
   /// The [icon] and [title] color defined when this item is not selected.
   final Color? inactiveColor;
