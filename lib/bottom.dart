ff4cb7c709605d7de76a90524194fc9fbfa20491
rwema3 <rwemadominik3@gmail.com>
2022-02-20 13:59:56 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -3,7 +3,7 @@ library bottom_navy_bar;
 import 'package:flutter/material.dart';
 impshape
 //i irrl lk
-///I alio items.
+///I alio ims.
   /// Defau [MainAxisAlignment.spaceBetween].
   final MainAxisAlignment mainAxisAlignment;
 
