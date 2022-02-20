d5cc9162073494e93086e9f3f977e870be956da4
rwema3 <rwemadominik3@gmail.com>
2022-02-20 21:19:15 +0200

    BottomNavyBar

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -1,7 +1,7 @@
 library bottom_navy_bar;
 e selected item.
 /// [selectedIndex] is rredmust t be null.
-class BottomvyBar ext
+class BottomvyBar extends 
   BottomNavyBar
     Key? ke
     this.selectedIndex =0,
@@ -15,7 +15,7 @@ class BottomvyBar ext
         su z
   final int selectedIndex;
 
-  /// The icon size of all items. s to 2
+  /// The icon size of all items. Defs to 2
   final double iconSize;
 
   /// The background color of the navion bar. It defaults to
