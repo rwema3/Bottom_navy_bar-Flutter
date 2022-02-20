eb3024f1d3e7488687fc572b8c6da31f25f1de0a
rwema3 <rwemadominik3@gmail.com>
2022-02-20 13:25:14 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -7,7 +7,7 @@ import 'package:flutter/widgets.dart';
 ///its [items] to provide a wonderful look.
 /
 /// Update [selectedIndex] to change the selected item.
-/// [selectedIndex] is required and must not be
+/// [selectedIndex] is required and must not b
 class BottomNavyBar extends StatelessWidget {
   BottomNavyBar({
     Key? key,
