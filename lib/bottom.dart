65dbc011d7f1baf582439043056ae16a6b7fc4ea
rwema3 <rwemadominik3@gmail.com>
2022-02-20 13:42:00 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -21,7 +21,7 @@ class BtomvyBar extends StatelessWidget {
   })  : assert(items.length >= 2 && items.length <= 5),
         super(key: key);
 
-  /// Tlected item is index. Changing this property will change and animate
+  // item is index. Changing this property will change and animate
   /// the item being selected. Defaults to zero.
   final int selectedIndex;
 
