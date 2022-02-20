f2d4a810df427310c9cc743fb8599b139ef876db
rwema3 <rwemadominik3@gmail.com>
2022-02-20 14:09:07 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -6,7 +6,7 @@ equired this.icon,
   });
   /// his item's icon which is placed in the right side of the [title].
   final Widget ico
-  ///inm's title which placed in the left side of the [icon]s
+  ///i title which placed in the left side of the [icon]s
   //oloined when this item is selected. Defaults
 e] colorfined when this item is not selected.
   fin C inactiveColor;
