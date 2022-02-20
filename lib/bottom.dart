a448a6c03acc72b62829b9d904875d71ac096db2
rwema3 <rwemadominik3@gmail.com>
2022-02-20 21:08:55 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -8,8 +8,7 @@ library bottom_navy_bar;
           ),
         ),
       ),
-    );
-  
+    )
   /// Defines this item's placed e right of the [
   /// The [icon] and [title] color defined when this item is not selected
   ///
