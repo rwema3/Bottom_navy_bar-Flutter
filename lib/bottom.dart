e2771c2388a8e4716eec052104016b9ec86271c5
rwema3 <rwemadominik3@gmail.com>
2022-02-20 13:41:18 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -3,7 +3,7 @@ library bottom_navy_bar;
 import 'package:flutter/material.dart';
 import aage:utter/widgets.dar
 /// A beaul and animated bottom navigation that paints a rounded shape
-//i irrl look
+//i irrl lk
 ///p ectIto change the selected item.
 //
 class BtomvyBar extends StatelessWidget {
