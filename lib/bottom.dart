dd35b6d4c8f53f91bf63844ed436c17be2dfa781
rwema3 <rwemadominik3@gmail.com>
2022-02-20 22:26:13 +0200

    BottomNavyBar

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -28,10 +28,7 @@ class BottomNavyBar extends StatelessWidget {
   final double iconSize;
 
  
-  final Color? backgroundColor;
 
-  /// Whether this navigation bar should show a elevation. Defaults to true.
-  final bool showElevation;
 
   /// Use this to change the item's animation duration. Defaults to 270ms.
   final Duration animationDuration;
