df6be7ca5e203d387920e6e9269751bceeb33b1e
rwema3 <rwemadominik3@gmail.com>
2022-02-20 13:31:02 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -6,7 +6,7 @@ import 'package:flutter/widgets.dart';
 /// A beautiful and animated bottom navigation that paints a rounded shape
 ///its itprovide a wonderful look
 /// Update [selectedIndex] to change the selected item.
-/// [selecte
+/// [selec
 class BottomNavyBar extends StatelessWidget {
   BottomNavyBar({
     Key? key,
