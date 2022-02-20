818c80af93b09990921cf49bc2c0ce730344ccca
rwema3 <rwemadominik3@gmail.com>
2022-02-20 21:08:03 +0200

    Update bottom.dart

--- lib/bottom.dart
+++ lib/bottom.dart
@@ -12,7 +12,7 @@ library bottom_navy_bar;
   
   /// Defines this item's placed e right of the [
   /// The [icon] and [title] color defined when this item is not selected.
-  final 
+  
   /// The alignment f
   ///
   /// This will take effecta 
